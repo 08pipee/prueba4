0ee275787db4d4b36e1209330f66fdb8a0c6c903
APRENDIZ <APRENDIZ@BOGCPCDMF030308.FormacionSena.RED>
2024-03-15 19:02:50 -0500

    PRIMER

---
+++ personas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+           persona persona = new persona();
+           Console.WriteLine(persona.NOMBRE);
+           Console.WriteLine(persona.FECHANACIMIENTO);
+           Console.WriteLine(persona.SEXO);
+           Console.WriteLine(persona.COLORPIEL);
+           Console.WriteLine(persona.COLOROJOS);
+           Console.WriteLine(persona.Estatura);
+           Console.WriteLine(persona .EDAD);
+           Console.WriteLine(persona.PESO);
+
+
+            
+        }
+    }
+}
---
+++ personas/persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace persona
+{
+    internal class persona
+    {
+        public persona(string nombre, DateTime fechanacimiento)
+        {
+            NOMBRE = nombre;
+            FECHANACIMIENTO = fechanacimiento;
+        }
+        public persona() {}
+        private string nombre;
+        private DateTime fechanacimiento;
+        private string sexo;
+        private string color_piel;
+        private double estatura;
+        private int edad;
+        private double peso;
+
+
+
+        public string NOMBRE { get => NOMBRE; set => NOMBRE = value; }
+        public DateTime FECHANACIMIENTO { get => FECHANACIMIENTO; set => FECHANACIMIENTO = value;}
+        public string SEXO { get => SEXO; set => SEXO = value;}
+        public string COLORPIEL { get => COLORPIEL; set => COLORPIEL = value;}
+        public string COLOROJOS { get => COLOROJOS; set => COLOROJOS = value;}
+        public double Estatura { get => Estatura; set => Estatura = value;}
+        public int EDAD { get => EDAD; set => EDAD = value;}
+        public double PESO { get => PESO; set => PESO = value;}
+
+     
+    } 
+
+}
